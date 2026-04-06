f3c976eadda66a1bf117fe2f4d4f21d33d869bfa
Radhesh Pandey <radhesh185@gmail.com>
2026-04-06 15:05:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ import paymentRouter from './routes/payment.routes.js';
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}));
+cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
+})
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
